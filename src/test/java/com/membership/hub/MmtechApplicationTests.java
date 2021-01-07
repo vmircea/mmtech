9da3ae4e4c56098061a8a3c8823c3b3e605d003b
Mircea-Calin Vacariuc <36712406+vmircea@users.noreply.github.com>
2021-01-07 19:51:41 +0200

    Initial commit

---
+++ src/test/java/com/membership/hub/MmtechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.membership.hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmtechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
